9dd5950971a4c3c340ee5fd7f5758671155d9da6
MichaelMaydev <micheletumminia89@gmail.com>
2026-02-27 16:32:25 +0100

    Aggiunta Service + DTO

--- src/main/java/org/generation/italy/cinema/dto/FilmDTO.java
+++ src/main/java/org/generation/italy/cinema/dto/FilmDTO.java
@@ -1,4 +1,27 @@
 package org.generation.italy.cinema.dto;
 
+import org.generation.italy.cinema.model.entities.Actor;
+import org.generation.italy.cinema.model.entities.Director;
+import org.generation.italy.cinema.model.entities.Genre;
+import org.generation.italy.cinema.model.entities.Screening;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class FilmDTO {
+    private Integer id;
+    private String title;
+    private Integer durationMinutes;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer ageRating;
+    private String directorName;
+    private List<String> screenings;
+    private Set<String> actors;
+    private Set<String> genres;
+
+
 }
--- src/main/java/org/generation/italy/cinema/model/services/abstractions/iFilmService.java
+++ src/main/java/org/generation/italy/cinema/model/services/abstractions/iFilmService.java
@@ -1,15 +1,14 @@
 package org.generation.italy.cinema.model.services.abstractions;
 
-import org.generation.italy.cinema.model.entities.Actor;
-import org.generation.italy.cinema.model.entities.Director;
 import org.generation.italy.cinema.model.entities.Film;
-import org.generation.italy.cinema.model.entities.Genre;
+import org.springframework.data.domain.Page;
 
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface FilmService {
+public interface iFilmService {
     // restituisce tutti i film presenti
     List<Film> findFilmAll();
     //ricerca tramite id
@@ -18,16 +17,9 @@ public interface FilmService {
     boolean deleteFilmById(int id);
     // creazione film
     Film createFilm(Film film);
-    // ricerca per titolo ma con contains
-    List<Film> findFilmByTitle(String title);
     // modifica film
     boolean updateFilmById(int id);
     // ricerca per genere --repo
-    List<Film> findFilmByGenre(Genre genre);
-    //ricerca per attore --repo
-    List<Film> findFilmByActor(Actor actor);
-    //ricerca per regista --repo
-    List<Film> findFilmByDirector(Director director);
-    //film che sono disponibili per data -> proiezione --repo
     List<Film> findFilmByDateScreening(LocalDate date);
+    Page<Film> globalSearch(String query, Pageable pageable);
 }
