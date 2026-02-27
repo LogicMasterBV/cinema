cc4f156e5c92c879a78ee6735de971de88001c55
LogicMasterBV <bajramvisha@hotmail.com>
2026-02-27 16:04:13 +0000

    Crea interfaccia repository Ticket e User

--- src/main/java/org/generation/italy/cinema/model/repositories/abstractions/TicketRepository.java
+++ src/main/java/org/generation/italy/cinema/model/repositories/abstractions/TicketRepository.java
@@ -1,7 +1,39 @@
 package org.generation.italy.cinema.model.repositories.abstractions;
 
 import org.generation.italy.cinema.model.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    List<Ticket> findByBookingId(Integer bookingId);
+
+    Page<Ticket> findByScreeningId(Integer screeningId, Pageable pageable);
+
+    @Query("""
+        SELECT t
+        FROM Ticket t
+        JOIN t.booking b
+        JOIN b.user u
+        WHERE u.id = :userId
+        ORDER BY t.date DESC
+        """)
+    Page<Ticket> findByUserId(@Param("userId") Integer userId, Pageable pageable);
+
+    boolean existsByScreeningIdAndSeatId(Integer screeningId, Integer seatId);
+
+    Optional<Ticket> findByScreeningIdAndSeatId(Integer screeningId, Integer seatId);
+
+    @Query("""
+        SELECT t.seat.id
+        FROM Ticket t
+        WHERE t.screening.id = :screeningId
+        """)
+    List<Integer> findOccupiedSeatIds(@Param("screeningId") Integer screeningId);
 }
--- src/main/java/org/generation/italy/cinema/model/repositories/abstractions/UserRepository.java
+++ src/main/java/org/generation/italy/cinema/model/repositories/abstractions/UserRepository.java
@@ -1,7 +1,25 @@
 package org.generation.italy.cinema.model.repositories.abstractions;
 
 import org.generation.italy.cinema.model.entities.User;
+import org.generation.italy.cinema.model.entities.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    Page<User> findByRole(UserRole role, Pageable pageable);
+
+    Page<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(
+            String firstName,
+            String lastName,
+            Pageable pageable
+    );
 }
